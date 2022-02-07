03997a2bbaf2c37829d568e30d4fc2ef648bc0b2
Frall <frall8@hotmail.com>
2022-02-07 21:49:26 +0100

    latest

---
+++ app/routes/works/index.tsx
@@ -0,0 +1,33 @@
+import { Link, LoaderFunction, useLoaderData } from "remix";
+import * as deseoPost from "./deseo.mdx";
+import * as murmuroPost from "./murmuro.mdx";
+
+function postFromModule(mod: any) {
+  return {
+    slug: mod.filename.replace(/\.mdx?$/, ""),
+    ...mod.attributes.meta,
+  };
+}
+
+export const loader: LoaderFunction = () => {
+  return [postFromModule(deseoPost), postFromModule(murmuroPost)];
+};
+
+export default function BlogIndex() {
+  const posts = useLoaderData();
+  return (
+    <div>
+      <h2>Articles</h2>
+      <ul>
+        {posts.map((post: any) => (
+          <li key={post.slug}>
+            <Link to={post.slug}>{post.title}</Link>
+            {post.description ? (
+              <p className="m-0 lg:m-0">{post.description}</p>
+            ) : null}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
